67a94d80b7ea93205b07fee248849efe0a99d0a1
Gil Alves <gg.alves@campus.fct.unl.pt>
2019-11-14 16:15:38 +0000

    Lesson 100: Resource Deleting Endpoint.

--- src/index.js
+++ src/index.js
@@ -96,6 +96,20 @@ app.patch('/users/:id', async (req, res) => {
     }
 });
 
+// Deletes a user
+app.delete('/users/:id', async (req, res) => {
+    const _id = req.params.id;
+    try {
+        const user = await User.findByIdAndDelete(_id);
+        if (!user) {
+            return res.status(404).send({ error: 'User does not exist!' });
+        }
+        res.status(200).send(user);
+    } catch(error) {
+        res.status(500).send(error);
+    }
+});
+
 
 
 // Adds a new task
@@ -159,6 +173,41 @@ app.get('/tasks/:id', async (req, res) => {
         res.status(500).send(error);
     });
     */
+});
+
+// Updates a task
+app.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['completed'];
+    const isValidOperation = updates.every(update => allowedUpdates.includes(update));
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid update! '});
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+        if (!task) {
+            return res.status(404).send({ error: 'Task not found.' });
+        }
+        res.status(200).send(task);
+    } catch(error) {
+        res.status(400).send(error);
+    }
+
+});
+
+// Deletes a task
+app.delete('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id);
+        if (!task) {
+            return res.status(404).send({ error: 'Task not found.' });
+        }
+        res.status(200).send(task);
+    } catch(error) {
+        res.status(500).send(error);
+    }
 });
 
 
