8af10f14f9aac808b1f0da676765067f02f45be8
Gil Alves <gg.alves@campus.fct.unl.pt>
2019-11-15 11:30:44 +0000

    Lesson 107: JSON Web Tokens.

--- src/index.js
+++ src/index.js
@@ -39,3 +39,15 @@ const myFunction = async() => {
 myFunction();
 */
 
+
+const jwt = require('jsonwebtoken');
+
+const myFunction = async() => {
+    const token = jwt.sign({ _id: 'abc123' }, 'thisismynewcourse', { expiresIn: '7 days' });
+    console.log(token);
+
+    const data = jwt.verify(token, 'thisismynewcourse');
+    console.log(data);
+};
+
+myFunction();
