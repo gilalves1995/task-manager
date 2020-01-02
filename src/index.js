8ded919834da921d29d79e77b374179235be7116
Gil Alves <gg.alves@campus.fct.unl.pt>
2020-01-02 18:17:22 +0000

    Bug fixing 2.

--- src/index.js
+++ src/index.js
@@ -66,7 +66,7 @@ app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
 
